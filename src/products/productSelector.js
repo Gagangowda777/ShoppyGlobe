b8aaeece1bd4317660c1311731346f858aa7fb3b
Gagangowda777 <gowdagagan799@gmail.com>
2026-03-26 19:22:57 +0530

    set up react-redux

---
+++ src/products/productSelector.js
@@ -0,0 +1,15 @@
+
+export const selectAllProducts = (state) =>
+  state.products.products;
+
+export const selectSearchTerm = (state) =>
+  state.products.searchTerm;
+
+export const selectFilteredProducts = (state) => {
+  const products = state.products.products;
+  const searchTerm = state.products.searchTerm.toLowerCase();
+
+  return products.filter((product) =>
+    product.title.toLowerCase().includes(searchTerm)
+  );
+};
---
+++ src/products/productSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [], 
+  searchTerm: "",
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
+
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+    },
+  },
+});
+
+export const { setProducts, setSearchTerm } =
+  productSlice.actions;
+
+export default productSlice.reducer;
